8e2e1a2d95e138f1a848ecdacf4e76c390a01464
youkm1 <youkm0806@sookmyung.ac.kr>
2025-07-12 06:55:35 +0900

    fix local->dns

--- lib/server-api.ts
+++ lib/server-api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8081';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://matchalot.duckdns.org';
 
 export interface StudyMaterialSummary {
   id: number;
